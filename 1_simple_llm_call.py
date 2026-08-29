1eab1256868f6f5d79e6d5fb42e3f75e76f3dac2
Udit Kumar <udit56579@gmail.com>
2026-08-29 16:50:54 +0530

    1_simple_llm_call.py

--- 1_simple_llm_call.py
+++ 1_simple_llm_call.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
@@ -8,7 +8,7 @@ load_dotenv()
 # Simple one-line prompt
 prompt = PromptTemplate.from_template("{question}")
 
-model = ChatGoogleGenerativeAI()
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 parser = StrOutputParser()
 
 # Chain: prompt → model → parser
