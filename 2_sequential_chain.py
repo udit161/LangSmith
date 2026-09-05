92e78736d8c0870d57271224074d99fe10a0dbf0
Udit Kumar <udit56579@gmail.com>
2026-09-05 21:20:12 +0530

    2_sequential_chain.py

--- 2_sequential_chain.py
+++ 2_sequential_chain.py
@@ -16,10 +16,15 @@ prompt2 = PromptTemplate(
 )
 
 model = ChatOpenAI()
-
+model1 = ChatOpenAI(model_name='gpt-4o-mini', temperature=0.7)
+model2 = ChatOpenAI(model_name='gpt-4o-mini', temperature=0.5)
 parser = StrOutputParser()
 
-chain = prompt1 | model | parser | prompt2 | model | parser
+chain = prompt1 | model1 | parser | prompt2 | model1 | parser
+config = {
+    'tags' : ['llm_app', 'report_generation', 'summarization'],
+    'metadata': {'model1': 'gpt-4o-mini', 'model1-temp':0.7, 'parser': 'stroutputparser'}
+}
 
 result = chain.invoke({'topic': 'Unemployment in India'})
 
